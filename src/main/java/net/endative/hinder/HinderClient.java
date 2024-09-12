d2fb9eb461fa38d947f96d6f7fd70f975c374d41
Endative <endative@gmail.com>
2024-09-12 18:49:25 +0100

    Initial commit

---
+++ src/main/java/net/endative/hinder/HinderClient.java
@@ -0,0 +1,10 @@
+package net.endative.hinder;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HinderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
